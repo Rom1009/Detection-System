89e96d778839b66f7d915d0841b18824510d3ca7
thomas.d.nguyen <thomas.d.nguyen@terralogic.com>
2025-10-14 13:38:14 +0700

    Update: FE

--- frontend/src/app/(home)/page.tsx
+++ frontend/src/app/(home)/page.tsx
@@ -16,6 +16,7 @@ type PropsType = {
   }>;
 };
 
+
 export default async function Home({ searchParams }: PropsType) {
   const { selected_time_frame } = await searchParams;
   const extractTimeFrame = createTimeFrameExtractor(selected_time_frame);
