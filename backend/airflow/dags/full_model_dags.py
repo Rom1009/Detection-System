48a30504b1e6a52b42ba8d55c3992a019f08fb67
thomas.d.nguyen <thomas.d.nguyen@terralogic.com>
2025-11-19 15:52:25 +0700

    Push

--- backend/airflow/dags/full_model_dags.py
+++ backend/airflow/dags/full_model_dags.py
@@ -12,6 +12,7 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
     
+    
 
 import pendulum
 from airflow.sdk import dag
