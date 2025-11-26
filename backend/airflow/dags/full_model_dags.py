b7c8afed3b9685002287dd721c76aa72585be2fc
thomas.d.nguyen <thomas.d.nguyen@terralogic.com>
2025-11-26 14:04:11 +0700

    Feat: Create dags

--- backend/airflow/dags/full_model_dags.py
+++ backend/airflow/dags/full_model_dags.py
@@ -7,10 +7,11 @@ import pendulum
 from airflow import DAG  # Dùng class DAG gốc
 from airflow.providers.smtp.operators.smtp import EmailOperator
 from airflow.providers.docker.operators.docker import DockerOperator
-from airflow.providers.standard.operators.python import ShortCircuitOperator
+from airflow.providers.standard.operators.python import ShortCircuitOperator, PythonOperator
 from airflow.utils.context import Context
 from src.ml_pipeline.data_pipeline.data_collection import data_collection
 from docker.types import Mount
+from airflow.exceptions import AirflowException
 
 # Setup đường dẫn (Giữ nguyên)
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
@@ -27,7 +28,12 @@ def check_if_data_updated(**context):
     else:
         print("No new data")
         return False
-    
+
+def stop_pipeline_for_approval():
+    raise AirflowException(
+        "✋ DỪNG LẠI CHỜ GÁN NHÃN!\n"
+        "Vui lòng gán nhãn dữ liệu xong, quay lại đây bấm 'Mark as Success' để chạy tiếp bước DVC."
+    )
 
 # --- 3. DÙNG 'WITH DAG' ĐỂ TỰ ĐỘNG GÁN DAG CHO OPERATOR ---
 with DAG(
@@ -43,19 +49,20 @@ with DAG(
         auto_remove="success",
         mount_tmp_dir=False,
         do_xcom_push=True,
+        network_mode="bridge",
         command='python /app/ai/src/ml_pipline/data_pipeline/data_collection.py',
         environment={
             'RCLONE_CONFIG': '/etc/rclone.conf'
         },
         mounts=[
             Mount(
-                source='/home/thomas/.config/rclone/rclone.conf', 
+                source='/home/dinhquy/.config/rclone/rclone.conf', 
                 target='/etc/rclone.conf',     
                 type='bind',
                 read_only=True 
             ), 
             Mount(
-                source='/home/thomas/Desktop/AI/Detection-System/backend/public/labelling', 
+                source='/home/dinhquy/Desktop/Code/AI/Detection-System/backend/public/labelling', 
                 target='/app/ai/public/labelling', # Check lại log lỗi cũ để lấy đúng đường dẫn đích
                 type='bind'
             )
@@ -80,5 +87,61 @@ with DAG(
         """,
     )
     
+    wait_for_manual_labeling = PythonOperator(
+        task_id = "wait_for_manual_labeling",
+        python_callable = stop_pipeline_for_approval,
+        trigger_rule="all_success",
+        do_xcom_push=False
+    )
+    
+    # dvc_task = DockerOperator(
+    #     task_id = "dvc pull and dvc add",
+    #     api_version = "auto",
+    #     image = "dvc_tools:latest",
+    #     auto_remove = "success",
+    #     mount_tmp_dir=False,
+    #     network_mode="bridge",
+    #     command = "dvc pull && dvc add . && git add .", 
+    #     mounts=[
+    #         Mount(
+    #             source='/home/dinhquy/Desktop/Code/AI/Detection-System/.git', 
+    #             target='/etc/rclone.conf',     
+    #             type='bind',
+    #             read_only=True 
+    #         ), 
+    #         Mount(
+    #             source='/home/dinhquy/Desktop/Code/AI/Detection-System/.dvc', 
+    #             target='/etc/rclone.conf',     
+    #             type='bind',
+    #             read_only=True 
+    #         ), 
+    #     ]
+    # )
+    
+    
+    # machine_learning_pipeline = DockerOperator(
+    #     task_id = "machine learning pipeline", 
+    #     api_version = "auto",
+    #     image = "ml_pipeline:latest", 
+    #     auto_remove = "success",
+    #     mount_tmp_dir=False,
+    #     network_mode="bridge",
+    #     command = "dvc repro", 
+    #     mounts=[
+    #         Mount(
+    #             source='/home/dinhquy/Desktop/Code/AI/Detection-System/.git', 
+    #             target='/etc/rclone.conf',     
+    #             type='bind',
+    #             read_only=True 
+    #         ), 
+    #         Mount(
+    #             source='/home/dinhquy/Desktop/Code/AI/Detection-System/.dvc', 
+    #             target='/etc/rclone.conf',     
+    #             type='bind',
+    #             read_only=True 
+    #         ), 
+    #     ]
+    # )
+    
     # Định nghĩa luồng chạy
-    collect_data >> check_new_data >> task_send_label_request
+    collect_data >> check_new_data >> task_send_label_request >> wait_for_manual_labeling
